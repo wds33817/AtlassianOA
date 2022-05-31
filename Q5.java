c3d18eb0333869582080093c732e21942309326b
wds33817 <91014157+wds33817@users.noreply.github.com>
2022-05-31 15:49:00 +1000

    Update Q5.java
    pass test.

--- Q5.java
+++ Q5.java
@@ -1,6 +1,3 @@
-// This is the last question in 2020 OA from Atlassian
-
-
 package Atlassian;
 
 public class Q5 {
@@ -74,9 +71,71 @@ public class Q5 {
     All 1x1 sub-grids have a sum > 2, so the answer is 0.
      */
     public static int largestSubgrid(int[][] grid, int maxSum) {
-        
+        int n = grid.length;
+        int[][] sum = new int[n][n];
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    sum[0][0] = grid[0][0];
+                }
+                else if (i == 0) {
+                    sum[0][j] = sum[0][j - 1] + grid[0][j];
+                }
+                else if (j == 0) {
+                    sum[i][0] = sum[i - 1][0] + grid[i][0];
+                }
+                else {
+                    sum[i][j] = sum[i - 1][j] + sum[i][j - 1] + grid[i][j] - sum[i - 1][j - 1];
+                }
+                mx = Math.max(mx, grid[i][j]);
+            }
+        }
+        int ans = 0;
+        int l = 0, r = n;
+        while (l < r) {
+            int x = l + (r - l + 1) / 2;
+            int res = 0;
+            for (int i = x - 1; i < n; i++) {
+                for (int j = x - 1; j < n; j++) {
+                    int total = sum[i][j];
+                    if (i >= x) total -= sum[i - x][j];
+                    if (j >= x) total -= sum[i][j - x];
+                    if (i >= x && j >= x) total += sum[i - x][j - x];
+                    res = Math.max(res, total);
+                }
+            }
+            if (maxSum >= res) l = x;
+            else r = x - 1;
+        }
+        System.out.println(r);
+        return r;
     }
     public static void main(String[] args) {
-
+        /*
+        int[][] grid = new int[2][2];
+        grid[0][0] = 4;
+        grid[0][1] = 5;
+        grid[1][0] = 6;
+        grid[1][1] = 7;
+        */
+        int[][] grid = new int[4][4];
+        grid[0][0] = 1;
+        grid[0][1] = 1;
+        grid[0][2] = 1;
+        grid[0][3] = 1;
+        grid[1][0] = 2;
+        grid[1][1] = 2;
+        grid[1][2] = 2;
+        grid[1][3] = 2;
+        grid[2][0] = 3;
+        grid[2][1] = 3;
+        grid[2][2] = 3;
+        grid[2][3] = 3;
+        grid[3][0] = 4;
+        grid[3][1] = 4;
+        grid[3][2] = 4;
+        grid[3][3] = 81;
+        largestSubgrid(grid, 39);
     }
 }
